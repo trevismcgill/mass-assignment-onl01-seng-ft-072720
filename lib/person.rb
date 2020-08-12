364b1e8ed85706c8665c6239768f4e2f83b9c4c0
Trevis McGill <trevismcgill25@gmail.com>
2020-08-12 01:50:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(argument)
-
+def initialize(attributes)
+attributes.each {|key, value| self.send(("#{key}="), value)}
 end
 
 
